2f5230bc3e92e1b31e90be9812581ae3997b8a27
Kari Oikarinen <kari.oikarinen@qt.io>
2016-11-08 16:23:29 +0200

    Add qdbd to the images
    It's only included in internal builds, since the repository is not
    public.
    
    qdbd not started by default, ADB is still the default. It can be started
    with `systemctl start qdbd`. It can be started in future boots with
    `systemctl enable qdbd`.
    
    Task-number: QTBUG-56069
    Change-Id: I71340cdb31de823e1375986d34f2f07e7be5971b
    Reviewed-by: Samuli Piippo <samuli.piippo@qt.io>

---
+++ recipes-qt/b2qt-addons/qdbd.bb
@@ -0,0 +1,79 @@
+############################################################################
+##
+## Copyright (C) 2016 The Qt Company Ltd.
+## Contact: https://www.qt.io/licensing/
+##
+## This file is part of the Boot to Qt meta layer.
+##
+## $QT_BEGIN_LICENSE:GPL$
+## Commercial License Usage
+## Licensees holding valid commercial Qt licenses may use this file in
+## accordance with the commercial license agreement provided with the
+## Software or, alternatively, in accordance with the terms contained in
+## a written agreement between you and The Qt Company. For licensing terms
+## and conditions see https://www.qt.io/terms-conditions. For further
+## information use the contact form at https://www.qt.io/contact-us.
+##
+## GNU General Public License Usage
+## Alternatively, this file may be used under the terms of the GNU
+## General Public License version 3 or (at your option) any later version
+## approved by the KDE Free Qt Foundation. The licenses are as published by
+## the Free Software Foundation and appearing in the file LICENSE.GPL3
+## included in the packaging of this file. Please review the following
+## information to ensure the GNU General Public License requirements will
+## be met: https://www.gnu.org/licenses/gpl-3.0.html.
+##
+## $QT_END_LICENSE$
+##
+############################################################################
+
+DESCRIPTION = "Qt Debug Bridge Daemon"
+SECTION = "devel"
+LICENSE = "The-Qt-Company-DCLA-2.1"
+LIC_FILES_CHKSUM = "file://qdbd/main.cpp;md5=37093977d3f09e6366def8955c8c71e6;beginline=1;endline=18"
+
+inherit qmake5
+
+SRC_URI = "git://codereview.qt-project.org/tqtc-boot2qt/qdb;branch=${BRANCH};protocol=ssh \
+           file://defaults \
+           file://qdbd.service \
+           file://qdbd-init.sh \
+          "
+
+SRCREV = "02a7d1ab030b83feb3300df5149b9ec0518975f4"
+BRANCH = "master"
+PV = "0.1+git${SRCPV}"
+
+DEPENDS = "qtbase"
+RRECOMMENDS_${PN} += "kernel-module-usb-f-fs kernel-module-usb-f-rndis"
+
+S = "${WORKDIR}/git"
+
+EXTRA_QMAKEVARS_PRE = "CONFIG+=daemon_only"
+
+do_configure_append() {
+    if [ -n "${ADB_PRODUCTID}" ]; then
+        sed -i -e 's/PRODUCT=.*/PRODUCT=${ADB_PRODUCTID}/' ${WORKDIR}/defaults
+    fi
+}
+
+do_install_append() {
+    install -m 0755 ${WORKDIR}/qdbd-init.sh ${D}${bindir}/
+    install -m 0755 -d ${D}${sysconfdir}/init.d
+    ln -s ${bindir}/qdbd-init.sh ${D}${sysconfdir}/init.d/
+
+    install -m 0755 -d ${D}${systemd_unitdir}/system
+    install -m 0644 ${WORKDIR}/qdbd.service ${D}${systemd_unitdir}/system/
+
+    install -m 0755 -d ${D}${sysconfdir}/default
+    install -m 0644 ${WORKDIR}/defaults ${D}${sysconfdir}/default/qdbd
+}
+
+INITSCRIPT_NAME = "qdbd-init.sh"
+INITSCRIPT_PARAMS = "defaults 96"
+
+SYSTEMD_SERVICE_${PN} = "qdbd.service"
+# adbd is started by default instead of qdbd
+SYSTEMD_AUTO_ENABLE = "disable"
+
+inherit update-rc.d systemd
---
+++ recipes-qt/b2qt-addons/qdbd/qdbd-init.sh
@@ -0,0 +1,117 @@
+#! /bin/sh
+###############################################################################
+## Copyright (C) 2016 The Qt Company Ltd.
+## Contact: http://www.qt.io/licensing/
+##
+## This file is part of the Boot to Qt meta layer.
+##
+## $QT_BEGIN_LICENSE:BSD$
+## You may use this file under the terms of the BSD license as follows:
+##
+## "Redistribution and use in source and binary forms, with or without
+## modification, are permitted provided that the following conditions are
+## met:
+##   * Redistributions of source code must retain the above copyright
+##     notice, this list of conditions and the following disclaimer.
+##   * Redistributions in binary form must reproduce the above copyright
+##     notice, this list of conditions and the following disclaimer in
+##     the documentation and/or other materials provided with the
+##     distribution.
+##   * Neither the name of The Qt Company Ltd nor the names of its
+##     contributors may be used to endorse or promote products derived
+##     from this software without specific prior written permission.
+##
+##
+## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+## "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+## LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+## A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+## OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+## SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+## LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+## DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+## THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+## (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+## OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
+##
+## $QT_END_LICENSE$
+###############################################################################
+MANUFACTURER="The Qt Company"
+PRODUCT_STRING="Boot2Qt Ethernet/RNDIS connection"
+
+DAEMON=/usr/bin/qdbd
+CONFIGFS_PATH=/sys/kernel/config
+
+GADGET_CONFIG=$CONFIGFS_PATH/usb_gadget/g1
+
+. /etc/default/qdbd
+
+function initialize_gadget() {
+    # Initialize gadget with first UDC driver
+    for driverpath in /sys/class/udc/*; do
+        drivername=`basename $driverpath`
+        echo "$drivername" > $GADGET_CONFIG/UDC
+        break
+    done
+}
+
+function disable_gadget() {
+    echo "" > $GADGET_CONFIG/UDC
+}
+
+case "$1" in
+start)
+    modprobe libcomposite
+    # Gadget configuration
+    mkdir -p $GADGET_CONFIG
+    echo $VENDOR > $GADGET_CONFIG/idVendor
+    echo $PRODUCT > $GADGET_CONFIG/idProduct
+    mkdir -p $GADGET_CONFIG/strings/0x409
+    echo $MANUFACTURER > $GADGET_CONFIG/strings/0x409/manufacturer
+    echo $PRODUCT_STRING > $GADGET_CONFIG/strings/0x409/product
+    echo ${SERIAL:0:32} > $GADGET_CONFIG/strings/0x409/serialnumber
+    mkdir -p $GADGET_CONFIG/configs/c.1/strings/0x409
+    echo "USB Ethernet + QDB" > $GADGET_CONFIG/configs/c.1/strings/0x409/configuration
+    mkdir -p $GADGET_CONFIG/functions/rndis.usb0
+    mkdir -p $GADGET_CONFIG/functions/ffs.qdb
+    ln -sf $GADGET_CONFIG/functions/rndis.usb0 $GADGET_CONFIG/configs/c.1
+    ln -sf $GADGET_CONFIG/functions/ffs.qdb $GADGET_CONFIG/configs/c.1
+    # Function fs mountpoints
+    mkdir -p /dev/usb-ffs
+    chmod 770 /dev/usb-ffs
+    mkdir -p /dev/usb-ffs/qdb
+    chmod 770 /dev/usb-ffs/qdb
+    mount -t functionfs qdb /dev/usb-ffs/qdb -o uid=0,gid=0
+    shift
+    start-stop-daemon --start --quiet --exec $DAEMON -- $@ &
+    sleep 1
+    initialize_gadget
+    ;;
+stop)
+    disable_gadget
+    start-stop-daemon --stop --quiet --exec $DAEMON
+    sleep 1
+    umount /dev/usb-ffs/qdb
+    rm $GADGET_CONFIG/configs/c.1/rndis.usb0
+    rm $GADGET_CONFIG/configs/c.1/ffs.qdb
+    rmdir $GADGET_CONFIG/configs/c.1/strings/0x409
+    rmdir $GADGET_CONFIG/configs/c.1
+    rmdir $GADGET_CONFIG/functions/rndis.usb0
+    rmdir $GADGET_CONFIG/functions/ffs.qdb
+    rmdir $GADGET_CONFIG/strings/0x409
+    rmdir $GADGET_CONFIG
+    ;;
+restart)
+    disable_gadget
+    start-stop-daemon --stop --quiet --exec $DAEMON
+    sleep 1
+    shift
+    start-stop-daemon --start --quiet --exec $DAEMON -- $@ &
+    sleep 1
+    initialize_gadget
+    ;;
+*)
+    echo "Usage: $0 {start|stop|restart}"
+    exit 1
+esac
+exit 0
