69e583faaf509cfbd021c9a0f948a14c61436377
Samuli Piippo <samuli.piippo@qt.io>
2019-04-15 13:34:24 +0300

    Upload correct mingw toolchain
    Use the environment variable to select correct package to upload.
    
    Change-Id: I687d05329d5f4408530808144894548b4474ca60
    Reviewed-by: Mikko Gronoff <mikko.gronoff@qt.io>

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -36,10 +36,10 @@ UPLOADPATH=QT@ci-files02-hki.intra.qt.io:/srv/jenkins_data/enterprise/b2qt/yocto
 UPLOADS="\
     tmp/deploy/images/${MACHINE}/b2qt-${PROJECT}-qt5-image-${MACHINE}.7z \
     tmp/deploy/sdk/b2qt-x86_64-meta-toolchain-b2qt-${PROJECT}-qt5-sdk-${MACHINE}.sh \
-    tmp/deploy/sdk/b2qt-i686-mingw32-meta-toolchain-b2qt-${PROJECT}-qt5-sdk-${MACHINE}.7z \
+    tmp/deploy/sdk/b2qt-${MINGW}-meta-toolchain-b2qt-${PROJECT}-qt5-sdk-${MACHINE}.7z \
     tmp/deploy/sdk/b2qt-x86_64-meta-toolchain-b2qt-embedded-sdk-${MACHINE}.sh \
     tmp/deploy/qbsp/meta-b2qt-${PROJECT}-qbsp-x86_64-${MACHINE}-${RELEASE}.qbsp \
-    tmp/deploy/qbsp/meta-b2qt-${PROJECT}-qbsp-i686-mingw32-${MACHINE}-${RELEASE}.qbsp \
+    tmp/deploy/qbsp/meta-b2qt-${PROJECT}-qbsp-${MINGW}-${MACHINE}-${RELEASE}.qbsp \
     "
 
 for f in ${UPLOADS}; do
