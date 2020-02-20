varigit/meta-variscite-boot2qt

commit dbb103e486530ef2db5a86bc627cc7be67a2f2ee

toradex: update layers to 3.0b3.118-20200101
Also minor cleanup with removing colibri-vf from machines list &
update tezi json override to match the updated definition
in meta-toradex-bsp-common/classes/image_type_tezi.bbclass

Task-number: QTBUG-81891
Change-Id: If4028d97ae82410c42ec464f783edb33140075ee
Reviewed-by: Samuli Piippo <samuli.piippo@qt.io>

meta-boot2qt/classes/image_types_sdcard.bbclass
@@ -1,6 +1,6 @@
 ############################################################################
 ##
-## Copyright (C) 2018 The Qt Company Ltd.
+## Copyright (C) 2020 The Qt Company Ltd.
 ## Contact: https://www.qt.io/licensing/
 ##
 ## This file is part of the Boot to Qt meta layer.
@@ -93,16 +93,13 @@ def rootfs_tezi_json_b2qt(d, flash_type, flash_data, json_file, uenv_file):
     qtversion = subprocess.check_output(['qmake', '-query', 'QT_VERSION']).decode('utf-8').strip()
 
     deploydir = d.getVar('DEPLOY_DIR_IMAGE')
-    # Patched in IMAGE_CMD_teziimg() below
-    release_date = "%release_date%"
-
     data = OrderedDict({ "config_format": 2, "autoinstall": False })
 
     # Use image recipes SUMMARY/DESCRIPTION/PV...
     data["name"] = d.getVar('SUMMARY')
     data["description"] = d.getVar('DESCRIPTION')
     data["version"] = "Qt " + qtversion
-    data["release_date"] = release_date
+    data["release_date"] = datetime.strptime(d.getVar('DATE', False), '%Y%m%d').date().isoformat()
     data["u_boot_env"] = uenv_file
     if os.path.exists(os.path.join(deploydir, "prepare.sh")):
         data["prepare_script"] = "prepare.sh"

meta-boot2qt-distro/dynamic-layers/toradex-nxp-layer/recipes-bsp/u-boot/u-boot-script-toradex.bb
@@ -1,6 +1,6 @@
 ############################################################################
 ##
-## Copyright (C) 2018 The Qt Company Ltd.
+## Copyright (C) 2020 The Qt Company Ltd.
 ## Contact: https://www.qt.io/licensing/
 ##
 ## This file is part of the Boot to Qt meta layer.
@@ -31,7 +31,7 @@ LICENSE = "The-Qt-Company-Commercial"
 LIC_FILES_CHKSUM = "file://${BOOT2QTBASE}/licenses/The-Qt-Company-Commercial;md5=c8b6dd132d52c6e5a545df07a4e3e283"
 DEPENDS = "u-boot-mkimage-native"
 
-PV = "3.0b2"
+PV = "3.0b3"
 
 SRC_URI = " \
     file://fwd_mmc.scr \
@@ -69,4 +69,4 @@ do_compile[noexec] = "1"
 do_install[noexec] = "1"
 
 PACKAGE_ARCH = "${MACHINE_ARCH}"
-COMPATIBLE_MACHINE = "(apalis-imx6|colibri-vf|colibri-imx6|colibri-imx7)"
+COMPATIBLE_MACHINE = "(apalis-imx6|colibri-imx6|colibri-imx7)"

scripts/manifest.xml
@@ -72,11 +72,11 @@
           fetch="git://git2.toradex.com"/>
   <project name="meta-toradex-bsp-common"
            remote="toradex"
-           revision="d7265604f72ddcc5ca1c443847c1244952700f2d"
+           revision="ac6df0b7458c551e915cf186481c23401a4b6824"
            path="sources/meta-toradex-bsp-common"/>
   <project name="meta-toradex-nxp"
            remote="toradex"
-           revision="ba278cfe4fa79db91727b783f7f900ab72833d36"
+           revision="7d996d197ad3185838103da3229e01f1b00285c4"
            path="sources/meta-toradex-nxp"/>
 
   <!-- BoundaryDevices -->

commit aff4b9908deb1dd2efc5432e9895b3992ee4234e

image_types_sdcard: update override
Update tezi override to fix tezi tar package creation (broken by
commit dbb103e486530ef2db5a86bc627cc7be67a2f2ee)

Change-Id: I1749b87933844c6c328712e847fe47885cebe5fe
Reviewed-by: Samuli Piippo <samuli.piippo@qt.io>

meta-boot2qt/classes/image_types_sdcard.bbclass
@@ -81,8 +81,8 @@ END
 
 do_image[depends] += "qtbase-native:do_populate_sysroot"
 IMAGE_CMD_teziimg_append() {
-    ${IMAGE_CMD_TAR} --transform 's,^,${IMAGE_NAME}-Tezi_${PV}/,' -rhf ${IMGDEPLOYDIR}/${IMAGE_NAME}-Tezi_${PV}${TDX_VERDATE}.tar TEZI_B2QT_EULA.TXT Built_with_Qt.png
-    ln -fs ${IMAGE_NAME}-Tezi_${PV}${TDX_VERDATE}.tar ${IMGDEPLOYDIR}/${IMAGE_LINK_NAME}.tezi.tar
+    ${IMAGE_CMD_TAR} --transform 's,^,${IMAGE_NAME}-Tezi_${PV}/,' -rhf ${IMGDEPLOYDIR}/${IMAGE_NAME}-Tezi_${PV}-${DATE}.tar TEZI_B2QT_EULA.TXT Built_with_Qt.png
+    ln -fs ${IMAGE_NAME}-Tezi_${PV}-${DATE}.tar ${IMGDEPLOYDIR}/${IMAGE_LINK_NAME}.tezi.tar
 }
 
 def rootfs_tezi_json_b2qt(d, flash_type, flash_data, json_file, uenv_file):